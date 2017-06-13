f5764f9224b9aee21f90fbfc76ba38b039d63c9a
bekkamaditya <bekkamvenkata.aditya@nutanix.com>
2017-06-13 16:33:56 +0530

    First commit

---
+++ client.py
@@ -0,0 +1,50 @@
+import app_pb2
+import app_pb2_grpc
+import grpc
+import sys
+import time
+
+def getResponse( stub ):
+    request = app_pb2.AppRequest()
+    request.name = raw_input("Enter your name:")
+    response = stub.GetResponse(request)
+    print response.email
+
+def getStreamedResponse( stub ):
+    request = app_pb2.AppRequest()
+    request.name = raw_input("Enter your name:")
+    responses = stub.GetStreamedResponse(request)
+    for response  in responses:
+	print response.email
+
+def getResponseIterator(stub): 
+    print "Enter the list of names followed by a empty line: "
+    while True:
+	name = raw_input()
+	if name == '' :
+	    break
+	request = app_pb2.AppRequest()
+	request.name = name
+	#yield app_pb2.AppRequest(name)
+	yield request
+
+def getResponse1(stub):
+    request_list = getResponseIterator(stub)
+    response = stub.GetResponse1(request_list)
+    print response 
+        
+def multipleExchange(stub):
+    request_list = getResponseIterator(stub)    
+    responses = stub.MultipleStream(request_list)
+    for response  in responses:
+	print response.email
+
+def run():
+    channel = grpc.insecure_channel('localhost: 50051') 
+    stub = app_pb2_grpc.ApplicationStub(channel)
+    getStreamedResponse( stub )  
+    getResponse1(stub) 
+    multipleExchange(stub)
+
+if __name__ == '__main__' :
+    run()
---
+++ server.py
@@ -0,0 +1,53 @@
+import app_pb2 
+import app_pb2_grpc
+import grpc 
+from concurrent import futures
+import sys
+import time
+
+_ONE_DAY_IN_SECONDS = 24*60*60
+
+def getResponse( request ):
+    return app_pb2.AppResponse(email='{}@nutanix.com'.format(request.name)) 
+
+class ApplicationServicer( app_pb2_grpc.ApplicationServicer ):
+    def __init__(self):
+        print "Server started"
+
+    def GetResponse( self,request,context ):
+        response = getResponse(request)
+        #print( response )
+	return response
+
+    def GetStreamedResponse(self, request, context):
+	for i in range(10) :
+	    yield app_pb2.AppResponse(email='{}-{}@nutanix.com'.format(request.name,i))		
+
+    def GetResponse1( self,request_iterator,context):
+	name = "" 
+	for request in request_iterator	:
+	    name = name + request.name + '-' 
+	#print name
+	return app_pb2.AppResponse(email='{}@nutanix.com'.format(name)) 
+
+    def MultipleStream(self,request_iterator,context):
+	name = "" 
+	for request in request_iterator	:
+	    yield app_pb2.AppResponse(email='{}@nutanix.com'.format(request.name)) 
+		
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    app_pb2_grpc.add_ApplicationServicer_to_server(ApplicationServicer(),server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    serve()
+
